6daa6c967d1bc8dea2ff59677fa0254325f38e17
nik00726 <nik00726@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-05 14:10:54 +0000

    html update
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-gallery/trunk@833309 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-photo-gallery.php
+++ wp-photo-gallery.php
@@ -723,6 +723,7 @@
                                 <th><span>Published On</span></th>
                                 <th><span>Edit</span></th>
                                 <th><span>Delete</span></th>
+                               </tr> 
                             </thead>
                             <tbody id="the-list">
                                 <?php
